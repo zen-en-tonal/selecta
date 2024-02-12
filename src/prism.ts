a646a0ab05b8fba0572a07c913272586a9178941
Takeru KODAMA <zen.en.tonal@gmail.com>
2024-02-13 07:33:42 +0900

    [Change] rename Prism into Spectrum

--- src/prism.ts
+++ src/prism.ts
@@ -6,11 +6,11 @@ type Named<T> = {
   [K in string]: T;
 };
 
-type Prism<T> = { [K in keyof Named<T>]: Scalar };
+type Spectrum<T> = { [K in keyof Named<T>]: Scalar };
 
 export function prism<T extends Lens<Q, Scalar>, Q>(
   lenses: Named<T>
-): (lay: Q) => Prism<typeof lenses> {
+): (lay: Q) => Spectrum<typeof lenses> {
   return (lay: Q) => {
     const obj: { [K in keyof typeof lenses]: Scalar } = {};
     for (const [k, v] of Object.entries(lenses)) {
