8c2ed7fd46cf9ab6597c8148585320bd0d7b5fdd
Takeru KODAMA <zen.en.tonal@gmail.com>
2024-02-22 07:24:09 +0900

    [Add] make `parseNumber` and `toNonNull` lenses to be exported

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
 export type { Schema } from "./src/prism.ts";
-export { combine, focus, lens } from "./src/lens.ts";
+export { combine, focus, lens, parseNumber, toNonNull } from "./src/lens.ts";
 export type { Record, Scalar } from "./src/lens.ts";
 export { fromSchema, prism } from "./src/prism.ts";
